c2d79780bffb6e61df81684f9cfee515f2c98752
Pete <peteburquette@gmail.com>
2023-02-07 03:51:56 +0000

    new prettier eslint standard

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,27 @@ module.exports = {
     env: {
         browser: true,
         es2020: true,
+        node: true,
     },
-    extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended',
-    ],
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
     overrides: [],
-    parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
     },
-    plugins: ['react', '@typescript-eslint'],
-    rules: {},
+    plugins: ['react'],
+    rules: {
+        'react/prop-types': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-filename-extension': [
+            1,
+            { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+        ],
+    },
+    settings: {
+        react: {
+            pragma: 'React',
+            version: 'detect',
+        },
+    },
 }
